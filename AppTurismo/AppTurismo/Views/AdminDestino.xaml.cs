9f180839bbd157f8bb44e1705f27b509cc27dc0e
SbasLpz <124542513+SbasLpz@users.noreply.github.com>
2023-12-17 16:36:14 -0600

    Add files via upload

---
+++ AppTurismo/AppTurismo/Views/AdminDestino.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using AppTurismo.ViewModels;
+using Xamarin.Forms;
+
+namespace AppTurismo.Views
+{	
+	public partial class AdminDestino : ContentPage
+	{	
+		public AdminDestino ()
+		{
+			InitializeComponent ();
+            BindingContext = new AdminDestinoVM();
+        }
+	}
+}
+
