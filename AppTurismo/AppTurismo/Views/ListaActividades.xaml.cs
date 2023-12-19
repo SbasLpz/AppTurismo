0bfcd1d775fddcb6d676f03a157cdcc222764ab4
kfskevin <kfskevin5@gmail.com>
2023-12-19 03:11:53 -0600

    Funcion para poder guardar y eliminar actividades usando json.

---
+++ AppTurismo/AppTurismo/Views/ListaActividades.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTurismo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaActividades : ContentPage
+    {
+        private const string ActividadesFileName = "actividades.json";
+
+        private List<string> actividades;
+        public ListaActividades(List<string> actividades)
+        {
+            InitializeComponent();
+            actividadesListView.ItemsSource = actividades;
+            actividades = new List<string>();
+            CargarActividadesDesdeArchivo();
+            MostrarActividades();
+        }
+
+        private void btnEliminarActividad_Clicked(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string actividadAEliminar = clickedButton.CommandParameter as string;
+
+            if (!string.IsNullOrEmpty(actividadAEliminar))
+            {
+
+                actividades.Remove(actividadAEliminar);
+                GuardarActividadesEnArchivo();
+                MostrarActividades();
+            }
+        }
+
+        private void GuardarActividadesEnArchivo()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ActividadesFileName);
+            string actividadesJson = JsonConvert.SerializeObject(actividades);
+            File.WriteAllText(filePath, actividadesJson);
+        }
+
+        private void MostrarActividades()
+        {
+            actividadesListView.ItemsSource = actividades;
+        }
+        private void CargarActividadesDesdeArchivo()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "actividades.json");
+
+            if (File.Exists(filePath))
+            {
+                string actividadesJson = File.ReadAllText(filePath);
+                actividades = JsonConvert.DeserializeObject<List<string>>(actividadesJson);
+            }
+        }
+    }
+}
--- AppTurismo/AppTurismo/Views/calendario.xaml.cs
+++ AppTurismo/AppTurismo/Views/calendario.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AppTurismo.ViewModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
 
 
 
@@ -19,16 +21,34 @@ namespace AppTurismo.Views
     public partial class calendario : ContentPage
     {
 
-       // private ObservableCollection<Actividad> actividades;
-        private Dictionary<DateTime,List<string>> actividadesPorFecha = new Dictionary<DateTime, List<string>>();
+        private Dictionary<DateTime, List<string>> actividadesPorFecha = new Dictionary<DateTime, List<string>>();
+
+        private ObservableCollection<string> _actividades = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Actividades
+        {
+            get { return _actividades; }
+            set
+            {
+                if (_actividades != value)
+                {
+                    _actividades = value;
+                    OnPropertyChanged(nameof(Actividades));
+                }
+            }
+        }
+        private ObservableCollection<string> actividades = new ObservableCollection<string>();
+
         public calendario()
         {
             InitializeComponent();
-              calendar.IsVisible = true;
-              inicializarActividades();
-              calendar.DateSelected += Calendario_DateSelected;
-              MostrarFechaSeleccionada();
-              calendar.MinimumDate = DateTime.Now.Date;
+            BindingContext = this;
+            calendar.IsVisible = true;
+            inicializarActividades();
+            calendar.DateSelected += Calendario_DateSelected;
+            MostrarFechaSeleccionada();
+            calendar.MinimumDate = DateTime.Now.Date;
+            actividadesListView.ItemsSource = actividades;
 
             Title = "Turismo";
 
@@ -41,12 +61,10 @@ namespace AppTurismo.Views
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
             });
 
-          
+
         }
 
 
-
-        
         private void Calendario_DateSelected(object sender, DateChangedEventArgs e)
         {
             MostrarFechaSeleccionada();
@@ -55,7 +73,7 @@ namespace AppTurismo.Views
 
         private void MostrarFechaSeleccionada()
         {
-            
+
             DateTime fechaSeleccionada = calendar.Date;
 
             var Actividades = ObtenerActividadesParaFecha(fechaSeleccionada);
@@ -65,41 +83,106 @@ namespace AppTurismo.Views
         private void inicializarActividades()
         {
             var actividadesFecha1 = new List<string>
-            {
-             "Pesca deportiva",
-             "Surf",
-             "Degustacion de comidas"
+             {
+                 "Pesca deportiva",
+                 "Surf",
+                 "Degustacion de comidas"
              };
 
             var actividadesFecha2 = new List<string>
-              {
-               "Senderismo",
-               "Buceo"
-               };
+             {
+                 "Senderismo",
+                  "Buceo"
+             };
 
-           
+
             actividadesPorFecha.Add(DateTime.Parse("2023-12-28"), actividadesFecha1);
             actividadesPorFecha.Add(DateTime.Parse("2023-12-30"), actividadesFecha2);
+
+
+
+            // Actualizar la fuente de datos del ListView
+            actividadesListView.ItemsSource = actividades;
+
         }
 
         private List<string> ObtenerActividadesParaFecha(DateTime fecha)
         {
 
-           
+
             if (actividadesPorFecha.TryGetValue(fecha, out var actividades))
             {
                 return actividades;
             }
             else
             {
-                
+
                 return new List<string> { "No hay actividades para esta fecha." };
             }
         }
 
+
+
+
+        private const string ActividadesFileName = "actividades.json";
+        private HashSet<string> actividadesAgregadasEnClick = new HashSet<string>();
         private void btnAddActividad_Clicked(object sender, EventArgs e)
         {
-            
+            Button clickedButton = (Button)sender;
+            string selectedActivity = clickedButton.CommandParameter as string;
+
+
+            if (actividadesAgregadasEnClick.Contains(selectedActivity))
+            {
+                DisplayAlert("Alerta", "Esa actividad ya fue agregada", "Aceptar");
+                return;
+            }
+
+            DateTime fechaActividad = actividadesPorFecha.FirstOrDefault(pair => pair.Value.Contains(selectedActivity)).Key;
+            actividadesAgregadasEnClick.Add(selectedActivity);
+            actividadesAgregadasEnClick.Add(fechaActividad.ToString());
+            actividades.Add(selectedActivity);
+            actividades.Add(fechaActividad.ToString());
+
+            (BindingContext as calendario)?.Actividades.Add(selectedActivity);
+
+            GuardarActividadesEnArchivo();
+            lblActividadSeleccionada.Text = $"Actividades seleccionadas: {string.Join(", ", actividades)}";
+            lblFechaSeleccionada.Text = $"Fecha: {fechaActividad.ToString("yyyy-MM-dd")}";
+        }
+
+
+        private void GuardarActividadesEnArchivo()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ActividadesFileName);
+            string actividadesJson = JsonConvert.SerializeObject(actividades);
+            File.WriteAllText(filePath, actividadesJson);
+        }
+
+        private List<string> CargarActividadesDesdeArchivo()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ActividadesFileName);
+
+            if (File.Exists(filePath))
+            {
+                string actividadesJson = File.ReadAllText(filePath);
+                List<string> actividadesGuardadas = JsonConvert.DeserializeObject<List<string>>(actividadesJson);
+
+                return actividadesGuardadas;
+            }
+
+            return new List<string>();
+        }
+
+        private async void MostrarActividadesPage(List<string> actividades)
+        {
+            await Navigation.PushModalAsync(new ListaActividades(actividades));
+        }
+
+        private void btnMostrarActividades_Clicked(object sender, EventArgs e)
+        {
+            List<string> actividadesGuardadas = CargarActividadesDesdeArchivo();
+            MostrarActividadesPage(actividadesGuardadas);
         }
     }
 }
